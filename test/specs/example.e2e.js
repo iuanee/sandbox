95a365c8603f8da737ca8d8439d437c27f0208b5
Danielle Lee <dlee@amplify.com>
2022-12-05 16:28:33 -0500

    update tests

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,11 +1,12 @@
 const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
+describe('event', () => {
+    it('should validate event', async () => {
         await LoginPage.open();
 
         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        browser.cdp('Network', 'enable');
         await expect(SecurePage.flashAlert).toBeExisting();
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!');

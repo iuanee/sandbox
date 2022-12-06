afb29cafb021d1f7dfd756398deae83e34328132
Danielle Lee <dlee@amplify.com>
2022-12-05 19:43:39 -0500

    update dep from v7 to v8

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,8 +1,8 @@
-const LoginPage = require('../pageobjects/login.page');
-const SecurePage = require('../pageobjects/secure.page');
+const LoginPage = require('../pages/login.page');
+const SecurePage = require('../pages/secure.page');
 
-describe('event', () => {
-    it('should validate event', async () => {
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
         await LoginPage.open();
 
         await LoginPage.login('tomsmith', 'SuperSecretPassword!');

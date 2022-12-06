a20c631f8e57490074b99aa553f7ebc448645e3d
Danielle Lee <dlee@amplify.com>
2022-12-05 21:19:14 -0500

    updated with devtools cdp example

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,15 +1,16 @@
-const LoginPage = require('../pages/login.page');
-const SecurePage = require('../pages/secure.page');
+const MainPage = require('../pages/main.page');
+const EventValidator = require('../support/eventValidator');
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open();
-
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        browser.cdp('Network', 'enable');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+describe('validate sent request', () => {
+    it('should validate request is sent when button is clicked', async () => {
+        await MainPage.open();
+        await MainPage.landingElement.waitForDisplayed();
+        EventValidator.enableCDP();
+        EventValidator.addEventListener();
+        await MainPage.requestOutputLink.scrollIntoView({ block: 'center', inline: 'center' });
+        await MainPage.requestOutputLink.waitForDisplayed();
+        await MainPage.requestOutputLink.click();
+        await browser.pause(10000);
     });
 });
 
